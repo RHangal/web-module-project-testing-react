cf58784fce699f55ebc1b24d4f278c285a134424
Rohan Hangal <rohanhangal@gmail.com>
2023-03-28 13:26:43 -0700

    scraping this project, there doesn't seem to be a way to import userEvent. looks as if dependencies are way out of date

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Show from './../Show';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders without errors', () => { });
+import Show from "./../Show";
 
-test('renders Loading component when prop show is null', () => { });
+const testShow = {
+  name: "Foo Bar Show",
+  summary: "Foo Bar Summary",
+  seasons: [
+    {
+      id: 0,
+      name: "season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "season 2",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders same number of options seasons are passed in', () => { });
+test("renders without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} />);
+  const loading = screen.queryByText("Fetching data...");
+  expect(loading).toBeInTheDocument;
+  const testLoading = screen.queryByTestId("loading-container");
+  expect(testLoading).toBeInTheDocument;
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+  const seasonOptions = screen.queryAllByTestId("season-option");
+  expect(seasonOptions).toHaveLength(2);
+});
+
+test("handleSelect is called when an season is selected", async () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+});
+
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
